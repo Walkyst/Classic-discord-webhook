0cec947341d5485ed4e04ebd446585ba63a57e36
Walkyst <41806921+Walkyst@users.noreply.github.com>
2021-05-29 18:01:11 +0300

    bump deps

--- src/discord.js
+++ src/discord.js
@@ -2,7 +2,7 @@ const discord = require('discord.js')
 const MAX_MESSAGE_LENGTH = 50;
 
 module.exports.send = (id, token, repo, branch, url, commits, size) => new Promise((resolve, reject) => {
-    var client
+    let client;
     console.log("Preparing Webhook...")
     try {
         client = new discord.WebhookClient(id, token)
@@ -21,23 +21,23 @@ module.exports.send = (id, token, repo, branch, url, commits, size) => new Promi
 function createEmbed(repo, branch, url, commits, size) {
     console.log("Constructing Embed...");
 
-    return new discord.RichEmbed()
+    return new discord.embed
         .setColor("#7289DA")
         .setAuthor(`${branch.owner.name}`, `${branch.owner_url.avatar_url}`, `${branch.owner_url}`)
-        .setDescription(`[${repo.name}:${branch.name}] ${size} new ${size == 1 ? "commit" : "commits"}\n${getChangeLog(commits, size)}`)
+        .setDescription(`[${repo.name}:${branch.name}] ${size} new ${size === 1 ? "commit" : "commits"}\n${getChangeLog(commits, size)}`)
 }
 
 function getChangeLog(commits, size) {
-    var changelog = "";
-    for (var i in commits) {
+    let changelog = "";
+    for (const i in commits) {
         if (i > 11) {
             changelog += `+ ${size - i} more...\n`;
             break;
         }
 
-        var commit = commits[i];
-        var sha = commit.id.substring(0, 6);
-        var message = commit.message.length > MAX_MESSAGE_LENGTH ? (commit.message.substring(0, MAX_MESSAGE_LENGTH) + "...") : commit.message;
+        const commit = commits[i];
+        const sha = commit.id.substring(0, 6);
+        const message = commit.message.length > MAX_MESSAGE_LENGTH ? (commit.message.substring(0, MAX_MESSAGE_LENGTH) + "...") : commit.message;
         changelog += `[\`${sha}\`](${commit.url}) ${message} - ${commit.author.username}\n`
     }
 
