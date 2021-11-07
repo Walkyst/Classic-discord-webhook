515e97bbe722f0ba2461623ade7e6c66c29b01da
Walkyst <41806921+Walkyst@users.noreply.github.com>
2021-11-07 05:27:30 +0300

    Fix webhook for PR's events

--- src/discord.js
+++ src/discord.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js')
 const MAX_MESSAGE_LENGTH = 50;
 
-module.exports.send = (id, token, repository, branch, payload, commits, compare, size) => new Promise((resolve, reject) => {
+module.exports.send = (id, token, repository, branch, payload, commits, basesha, size) => new Promise((resolve, reject) => {
     let client;
     console.log("Preparing Webhook...")
     try {
@@ -12,20 +12,24 @@ module.exports.send = (id, token, repository, branch, payload, commits, compare,
         return
     }
 
-    client.send(createEmbed(repository, branch, payload, commits, compare, size)).then(() => {
+    client.send(createEmbed(repository, branch, payload, commits, basesha, size)).then(() => {
         console.log("Successfully sent the message!")
         resolve()
     }, reject)
 })
 
-function createEmbed(repository, branch, payload, commits, compare, size) {
+function createEmbed(repository, branch, payload, commits, basesha, size) {
     console.log("Constructing Embed...");
 
     return new discord.RichEmbed()
         .setColor("#7289DA")
-        .setAuthor(`${payload.repository.owner.name}`, `${payload.repository.owner.avatar_url}`, `${payload.repository.owner.html_url}`)
+        .setAuthor(`${payload.repository.owner.login}`, `${payload.repository.owner.avatar_url}`, `${payload.repository.owner.html_url}`)
         .setTitle(`[${repository.name}:${branch}] ${size} new ${size === 1 ? `commit` : `commits`}`)
-        .setURL(`${compare}`)
+        .setURL(`https://github.com/Walkyst/Minerea/compare/${basesha.substring(0, 12)}...${
+                commits[commits.length - 1].sha ?
+                    commits[commits.length - 1].sha.substring(0, 12) : commits[commits.length - 1].id.substring(0, 12)
+            }`
+        )
         .setDescription(`${getChangeLog(commits, size)}`)
 }
 
@@ -38,9 +42,12 @@ function getChangeLog(commits, size) {
         }
 
         const commit = commits[i];
-        const sha = commit.id.substring(0, 7);
-        const message = commit.message.length > MAX_MESSAGE_LENGTH ? (commit.message.substring(0, MAX_MESSAGE_LENGTH) + "...") : commit.message;
-        changelog += `[\`${sha}\`](${commit.url}) ${message} - ${commit.author.username}\n`
+        const author = commit.commit ? commit.commit.author.name : commit.author.name
+        const sha = commit.commit ? commit.sha.substring(0, 7) : commit.id.substring(0, 7)
+        const url = commit.commit ? commit.html_url : commit.url
+        const length = commit.commit ? commit.commit.message.length : commit.message.length
+        const message = length > MAX_MESSAGE_LENGTH ? ((commit.commit ? commit.commit.message.substring(0, MAX_MESSAGE_LENGTH) : commit.message.substring(0, MAX_MESSAGE_LENGTH)) + "...") : commit.commit ? commit.commit.message : commit.message;
+        changelog += `[\`${sha}\`](${url}) ${message} - ${author}\n`
     }
 
     return changelog;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-
+const axios = require('axios')
 const webhook = require('../src/discord.js')
 
 async function run() {
 	const payload = github.context.payload
+
+	let commits;
+	let branch;
+	let basesha;
+	if (payload.pull_request != null) {
+		const prcommits = await axios.get(payload.pull_request.commits_url,
+			{
+				headers: {
+					Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
+				}
+			}
+		)
+		commits = prcommits.data
+		branch = payload.pull_request.head.ref
+		basesha = payload.pull_request.base.sha
+	} else {
+		commits = payload.commits
+		branch = payload.ref.split('/')[payload.ref.split('/').length - 1]
+		basesha = payload.before
+	}
+
 	const repository = payload.repository
-	const commits = payload.commits
 	const size = commits.length
-	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1]
-	const compare = payload.compare
 
-	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`)
-
-	console.log(`Received ${commits.length}/${size} commits...`)
+	console.log(`Received ${size} commits...`)
 
 	const id = core.getInput("id")
 	const token = core.getInput("token")
 
-	webhook.send(id, token, repository, branch, payload, commits, compare, size).catch(err => core.setFailed(err.message));
+	webhook.send(id, token, repository, branch, payload, commits, basesha, size).catch(err => core.setFailed(err.message));
 }
 
 try {
-    run()
+	run()
 } catch (error) {
 	core.setFailed(error.message)
 }
